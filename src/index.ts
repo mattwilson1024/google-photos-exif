4810c51da47414afdbd8000c637f09a964fa2f0d
Matt Wilson <matt@matt-wilson.org>
2020-09-19 15:22:22 +0100

    Tidy up directory prep

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,17 @@ import { getAllFilesRecursively } from './helpers/get-all-files-recursively';
 const { readdir, mkdir, copyFile } = fspromises;
 
 interface Directories {
-  inputDir: string;
-  outputDir: string;
-  flattenedDir: string;
+  input: string;
+  output: string;
+  flattened: string;
 
-  untouchedDir: string;
-  adjustedDir: string;
-  metadataDir: string;
+  untouched: string;
+  modified: string;
+  json: string;
 }
 
 class GooglePhotosExif extends Command {
-  static description = 'Given a folder containing '
+  static description = `Takes in a directory containing the contents of a Google Photos Takeout and, for any images that are lacking metadata for the date/time the photo was taken, writes that date to the EXIF metadata from Google's JSON metadata. For gifs / movies, the _file_ modified date is adjusted to account for these formats not including EXIF metadata.`;
 
   static flags = {
     version: flags.version({char: 'v'}),
@@ -41,7 +41,8 @@ class GooglePhotosExif extends Command {
     const { inputDir, outputDir } = flags;
 
     try {
-      const directories = await this.setupDirectories(inputDir, outputDir);
+      const directories = this.determineDirectoryPaths(inputDir, outputDir);
+      await this.prepareDirectories(directories);
       await this.flattenAllFilesIntoSingleDirectory(directories);
     } catch (error) {
       this.error(error);
@@ -52,55 +53,59 @@ class GooglePhotosExif extends Command {
     this.exit(0);
   }
 
-  private async setupDirectories(inputDir: string, outputDir: string): Promise<Directories> {
-    const directories: Directories = {
-      inputDir,
-      outputDir,
-      flattenedDir: resolve(outputDir, 'flattened'),
-      untouchedDir: resolve(outputDir, 'untouched'),
-      adjustedDir: resolve(outputDir, 'adjusted'),
-      metadataDir: resolve(outputDir, 'metadata'),
+  private determineDirectoryPaths(inputDir: string, outputDir: string): Directories {
+    return {
+      input: inputDir,
+      output: outputDir,
+      flattened: resolve(outputDir, 'flattened'),
+      untouched: resolve(outputDir, 'untouched'),
+      modified: resolve(outputDir, 'modified'),
+      json: resolve(outputDir, 'json'),
     };
+  }
 
-    if (!inputDir || !existsSync(inputDir)) {
+  private async prepareDirectories(directories: Directories): Promise<void> {
+    if (!directories.input || !existsSync(directories.input)) {
       throw new Error('The input directory must exist');
     }
 
-    if (!outputDir) {
+    if (!directories.output) {
       throw new Error('You must specify an output directory using the --outputDir flag');
     }
 
-    const outputFolderExists = existsSync(outputDir);
+    const outputFolderExists = existsSync(directories.output);
     if (outputFolderExists) {
-      const outputFolderContents = await readdir(outputDir);
+      const outputFolderContents = await readdir(directories.output);
       const outputFolderContentsExcludingDSStore = outputFolderContents.filter(filename => filename !== '.DS_Store');
       const outputFolderIsEmpty = outputFolderContentsExcludingDSStore.length === 0;
       if (!outputFolderIsEmpty) {
         throw new Error('If the output directory already exists, it must be empty');
       }
     } else {
-      this.log(`Creating output directory: ${outputDir}`)
-      await mkdir(outputDir);
+      this.log(`Creating output root directory: ${directories.output}`);
+      await mkdir(directories.output);
     }
 
-    await mkdir(directories.flattenedDir);
-    await mkdir(directories.untouchedDir);
-    await mkdir(directories.adjustedDir);
-    await mkdir(directories.metadataDir);
-
-    return directories;
+    this.log(`Creating flattened, untouched, modified and json directories inside the output directory`);
+    await mkdir(directories.flattened);
+    await mkdir(directories.untouched);
+    await mkdir(directories.modified);
+    await mkdir(directories.json);
   }
 
   private async flattenAllFilesIntoSingleDirectory(directories: Directories): Promise<void> {
-    const allFiles = await getAllFilesRecursively(directories.inputDir);
+    this.log('--- Flattening all files from inputDir (including subdirectories) into a single directory ---')
+    const allFiles = await getAllFilesRecursively(directories.input);
     for (const srcFilePath of allFiles) {
       const srcFileName = basename(srcFilePath);
-      const destFilePath = resolve(directories.flattenedDir, srcFileName);
+      const destFilePath = resolve(directories.flattened, srcFileName);
 
       this.log(`Copying file: ${srcFilePath}`);
       await copyFile(srcFilePath, destFilePath);
     }
   }
+
+
 }
 
 export = GooglePhotosExif
