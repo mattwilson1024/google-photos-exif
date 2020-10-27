4d223c9e3b6401e677a602320af684d41e138bbb
Matt Wilson <matt@matt-wilson.org>
2020-10-27 21:04:33 +0000

    Support media files containing counter numbers e.g. foo(1).jpg should reference foo.jpg(1).json

--- src/helpers/get-companion-json-path-for-media-file.ts
+++ src/helpers/get-companion-json-path-for-media-file.ts
@@ -4,22 +4,41 @@ import { basename, dirname, extname, resolve } from 'path'
 export function getCompanionJsonPathForMediaFile(mediaFilePath: string): string|null {
   const directoryPath = dirname(mediaFilePath);
   const mediaFileExtension = extname(mediaFilePath);
-  const mediaFileNameWithoutExtension = basename(mediaFilePath, mediaFileExtension);
+  let mediaFileNameWithoutExtension = basename(mediaFilePath, mediaFileExtension);
 
   // Sometimes (if the photo has been edited inside Google Photos) we get files with a `-edited` suffix
-  // These won't have their own `.json` sidecar, but if we have a JSON file for the unedited version that'll be what we want
-  const mediaFileNameWithoutExtensionOrEditedSuffix = mediaFileNameWithoutExtension.replace(/[-]edited$/i, '');
+  // These images don't have their own .json sidecars - for these we'd want to use the JSON sidecar for the original image
+  // so we can ignore the "-edited" suffix if there is one
+  mediaFileNameWithoutExtension = mediaFileNameWithoutExtension.replace(/[-]edited$/i, '');
 
-  // The JSON files have varying extensions, sometimes it's just `.json` but sometimes it's more like `.jpg.json`
-  // Look to see if there is a JSON file with either naming pattern
-  const jsonPathIncludingMediaExt = resolve(directoryPath, `${mediaFileNameWithoutExtensionOrEditedSuffix}${mediaFileExtension}.json`);
-  const jsonPathExcludingMediaExt = resolve(directoryPath, `${mediaFileNameWithoutExtensionOrEditedSuffix}.json`);
+  // The naming pattern for the JSON sidecar files provided by Google Takeout seem to be inconsistent. For `foo.jpg`,
+  // the JSON file is sometimes `foo.json` but sometimes it's `foo.jpg.json`. Here we start building up a list of potential
+  // JSON filenames so that we can try to find them later
+  const potentialJsonFileNames: string[] = [
+    `${mediaFileNameWithoutExtension}.json`,
+    `${mediaFileNameWithoutExtension}${mediaFileExtension}.json`,
+  ];
 
-  if (existsSync(jsonPathIncludingMediaExt)) {
-    return jsonPathIncludingMediaExt;
-  } else if (existsSync(jsonPathExcludingMediaExt)) {
-    return jsonPathExcludingMediaExt;
-  } else {
-    return null;
+  // Another edge case which seems to be quite inconsistent occurs when we have media files containing a number suffix for example "foo(1).jpg"
+  // In this case, we don't get "foo(1).json" nor "foo(1).jpg.json". Instead, we strangely get "foo.jpg(1).json".
+  // We can use a regex to look for this edge case and add that to the potential JSON filenames to look out for
+  const nameWithCounterMatch = mediaFileNameWithoutExtension.match(/(?<name>.*)(?<counter>\(\d+\))$/);
+  if (nameWithCounterMatch) {
+    const name = nameWithCounterMatch?.groups?.['name'];
+    const counter = nameWithCounterMatch?.groups?.['counter'];
+    potentialJsonFileNames.push(`${name}${mediaFileExtension}${counter}.json`);
   }
+
+  // Now look to see if we have a JSON file in the same directory as the image for any of the potential JSON file names
+  // that we identified earlier
+  for (const potentialJsonFileName of potentialJsonFileNames) {
+    const jsonFilePath = resolve(directoryPath, potentialJsonFileName);
+    if (existsSync(jsonFilePath)) {
+      return jsonFilePath;
+    }
+  }
+
+  // If no JSON file was found, just return null - we won't be able to adjust the date timestamps without finding a
+  // suitable JSON sidecar file
+  return null;
 }
