68e8681ca1337a0abe7101a4caf53d9bfd9b6a3b
inakaboy <46212789+inakaboy@users.noreply.github.com>
2021-10-10 19:21:29 +0800

    added dng, m4v, webp, and sorted alphabetically

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,16 @@ import { Config } from './models/config-models';
 
 export const CONFIG: Config = {
   supportedMediaFileTypes: [
+    { extension: '.avi',  supportsExif: false },
+    { extension: '.dng', supportsExif: true },
+    { extension: '.gif',  supportsExif: false },
+    { extension: '.heic', supportsExif: true },
     { extension: '.jpeg', supportsExif: true },
     { extension: '.jpg',  supportsExif: true },
-    { extension: '.heic', supportsExif: true },
-    { extension: '.gif',  supportsExif: false },
+    { extension: '.m4v',  supportsExif: false },
+    { extension: '.mov',  supportsExif: true },
     { extension: '.mp4',  supportsExif: false },
-    { extension: '.png',  supportsExif: false },
-    { extension: '.avi',  supportsExif: false },
-    { extension: '.mov',  supportsExif: false },
+    { extension: '.png',  supportsExif: true },
+    { extension: '.webp',  supportsExif: true },    
   ],
 };
